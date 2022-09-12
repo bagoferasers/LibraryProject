be9133a6627fbb56c6f1f5b94c2be5b54b3655a8
bagoferasers <ctrull60@gmail.com>
2022-09-11 21:46:50 -0500

    Home Button 2
    Added home button for add book.

--- Home Library/src/application/controller/NewBookController.java
+++ Home Library/src/application/controller/NewBookController.java
@@ -1,9 +1,19 @@
 package application.controller;
 
+import java.io.File;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class NewBookController {
 
@@ -27,6 +37,21 @@ public class NewBookController {
 
     @FXML
     private Label BookAuthor;
+    
+    @FXML
+    private Button goHome;
 
-
+    @FXML
+    void goHome(ActionEvent event) {
+    	try {
+    		URL addBookURL = new File("MainMenu.fxml").toURI().toURL();
+			Parent root = FXMLLoader.load(addBookURL);
+			Scene scene = new Scene(root);
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			stage.setScene(scene);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
 }
