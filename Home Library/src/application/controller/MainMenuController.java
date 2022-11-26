9073c926ba46997216138415df541b0a9ac61fe2
bagoferasers <ctrull60@gmail.com>
2022-11-25 23:26:35 -0500

    Initializable
    Completed selected scene by adding initialize() that sets the searchList to the items previously searched when returning to the main screen.

--- Home Library/src/application/controller/MainMenuController.java
+++ Home Library/src/application/controller/MainMenuController.java
@@ -3,10 +3,13 @@ package application.controller;
 import application.model.Library;
 import java.io.File;
 import java.net.URL;
+import java.util.ResourceBundle;
+
 import application.model.Book;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -17,7 +20,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class MainMenuController {
+public class MainMenuController implements Initializable {
 
     @FXML
     private ListView< Book > searchList;
@@ -171,5 +174,13 @@ public class MainMenuController {
     		e.printStackTrace( );
     	}
     }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		if(!Library.searchedBooks.isEmpty()) {
+			searchList.setItems(Library.searchedBooks);
+		}
+			
+	}
 }
 
